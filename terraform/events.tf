cf3fb3a0958a550d93bf06bd6d084c8d58bb179c
EthanYee9 <ethanyee9@gmail.com>
2025-05-28 21:38:24 +0100

    disabled the scheduler tempoarily

--- terraform/events.tf
+++ terraform/events.tf
@@ -3,7 +3,7 @@ resource "aws_cloudwatch_event_rule" "scheduler" {
   name = "trigger_step_function"
   description = "tigger step function every 20 mins"
   schedule_expression = "rate(20 minutes)" 
-  state = "ENABLED"
+  state = "DISABLED" # temporarily disabled 
 }
 
 # scheduler targeting state machine 
