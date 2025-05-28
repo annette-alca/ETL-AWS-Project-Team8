022d245db11c9d41d34e01763a57105eba918072
Sonika Jha <sonikajha@Janoss-MacBook-Pro-2.local>
2025-05-28 17:02:48 +0100

    update lambda function

--- src/extract/utils.py
+++ src/extract/utils.py
@@ -1,9 +1,8 @@
 from pg8000.native import Connection
 import dotenv
 import os
-from pg8000.native import literal
 from datetime import datetime
-
+from decimal import Decimal
 
 def create_conn():
     dotenv.load_dotenv()
@@ -19,18 +18,11 @@ def create_conn():
     )
 
 
-def get_updated_data(db, table, last_extract):
-    tab_data = db.run(
-        f"""SELECT * FROM {literal(table)}
-                   WHERE last_updated > {literal(last_extract)};"""
-    )
-    keys = [column["name"] for column in db.columns]
-    new_dict = {key: tab_data[key] for key in keys}
-
-    return new_dict
-
-
-def serialise_datetime(obj):
+def serialise_object(obj):
     if isinstance(obj, datetime):
         return obj.isoformat()
+    elif isinstance(obj, Decimal):
+        return float(obj)
     raise TypeError("Type not serialisable")
+
+
