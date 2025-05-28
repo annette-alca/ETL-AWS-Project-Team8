c9796a494f2ae682169c7b63ce06292ddec21797
annette-alca <alca@me.com>
2025-05-28 12:26:49 +0100

    add connection and json files

--- src/extract/main.py
+++ src/extract/main.py
@@ -1,17 +1,42 @@
 import pg8000
+import json
+import boto3
+import os
 # from pg8000.native import literal
-from src.extract.connection import create_conn
+from src.extract.utils import *
+from datetime import datetime
 from pprint import pprint
 
-def get_all_data(db, table):
-    tab_data = db.run(f"SELECT * FROM {table} LIMIT 10;")
+def get_all_data(db, table_name):
+    tab_data = db.run(f"SELECT * FROM {table_name}")
+    extract_time = datetime.now()
     keys = [column["name"] for column in db.columns]
-    pprint(tab_data)
     new_dict_list = [{keys[i]:single_data[i] for i in range(len(keys))} for single_data in tab_data]
-    return new_dict_list
+    return new_dict_list, extract_time
+
+def get_updated_data(db, table_name, last_extract):
+    tab_data = db.run(f"""SELECT * FROM {table_name}
+                   WHERE last_updated > {last_extract};""")
+    extract_time = datetime.now()
+    keys = [column["name"] for column in db.columns]
+    new_dict_list = [{keys[i]:single_data[i] for i in range(len(keys))} for single_data in tab_data]
+    return new_dict_list, extract_time
+
+def save_to_tempdata(dict_list, extract_time, table_name):
+    date, time = str(extract_time).split()
+    path = f"tempdata/{table_name}/{date}/{table_name}_{time}.json"
+    with open(path,"w",encoding="utf-8") as f:
+        json.dump(dict_list,f, default= serialise_datetime, indent=2)
 
 
 if __name__ == "__main__":
     db = create_conn()
-    for table in ['department','staff','counterparty','transaction']:
-        pprint(get_all_data(db, table))
+    for table_name in ['department','staff','counterparty','transaction']:
+        try:
+            os.mkdir(f'tempdata/{table_name}')
+            os.mkdir(f'tempdata/{table_name}/2025-05-28')
+        except:
+            pass
+        tab_data, extract_time  = get_all_data(db,table_name)
+        save_to_tempdata(tab_data, extract_time, table_name)
+   
--- src/extract/utils.py
+++ src/extract/utils.py
@@ -1,6 +1,26 @@
-from pg8000 import literal
+from pg8000.native import Connection
+import dotenv
+import os
+from pg8000.native import literal
+from datetime import datetime
 
-def format_dict
+def create_conn():
+    dotenv.load_dotenv()
+
+    user = os.environ["DBUSER"]
+    database = os.environ["DBNAME"]
+    dbhost=os.environ["HOST"]
+    dbport=os.environ["PORT"]
+    password = os.environ["DBPASSWORD"]
+
+    return Connection(
+        database=database,
+        user=user,
+        password=password,
+        host = dbhost,
+        port = dbport
+    )
+
 
 def get_updated_data(db, table, last_extract):
     tab_data = db.run(f"""SELECT * FROM {literal(table)}
@@ -9,3 +29,8 @@ def get_updated_data(db, table, last_extract):
     new_dict = {key:tab_data[key] for key in keys}
 
     return new_dict
+
+def serialise_datetime(obj):
+    if isinstance(obj, datetime):
+        return obj.isoformat()
+    raise TypeError("Type not serialisable")
