0f3bf2b6815c4fef4938e454917deb476cf46d02
annette-alca <alca@me.com>
2025-05-28 11:01:25 +0100

    sql query folder for reference

---
+++ sql/queries.sql
@@ -0,0 +1,26 @@
+select * from address;
+
+select * from counterparty;
+
+select * from currency;
+
+select * from department;
+
+select * from  design
+limit 10;
+select * from  payment
+order by last_updated desc
+limit 10;
+select * from  payment_type
+limit 10;
+
+select * from purchase_order
+order by last_updated desc
+limit 10;
+
+select * from staff
+limit 10;
+
+select * from transaction
+order by last_updated desc
+limit 10;
