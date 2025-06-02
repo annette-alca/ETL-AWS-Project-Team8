b7ced551cee88b717ba0e9702a0b9cf977e00726
EthanYee9 <ethanyee9@gmail.com>
2025-06-02 16:09:02 +0100

    adding mvp_transform and save to s3 functions

--- src/transform/main.py
+++ src/transform/main.py
@@ -20,66 +20,65 @@ def lambda_transform(events, context):
     df_list = []
     ingestion_bucket = os.environ["INGESTION_S3"]
     processed_bucket = os.environ["PROCESSED_S3"]
+    list_of_keys = []
     for new_key in events["new_keys"]:
         table_name, new_df = append_json_raw_tables(s3_client, ingestion_bucket, new_key, processed_bucket)
-        
+        transformed_name, transformed_df = mvp_transform_df(s3_client, table_name, new_df, processed_bucket)
+        list_of_keys.append(save_to_s3(processed_bucket, transformed_df, transformed_name, events["timestamp"]))
 
-    #     response = s3_client.get_object(
-    #         Bucket="team-08-ingestion-20250528081548341900000001", 
-    #         Key=file
-    #     )
-    #     body = response["Body"].read().decode("utf-8")
-    #     df_list.append(pd.read_json(StringIO(body)))
-    # print(df_list)
-    
-    
-    # date_time = str(datetime.now())
+def key_to_df(s3_client, table_name, bucket_name):
+    new_json = s3_client.get_object(Bucket=bucket_name, Key=f"raw_data/{table_name}_all.json")
+    new_df = pd.read_json(StringIO(new_json["Body"].read().decode("utf-8")), orient="index")
+    new_df.to_csv("tempdata/dim_staff_test.csv")
+    return new_df
 
-    # file_path_staff = "tempdata/staff/2025-05-28/staff_12:25:18.184743.json"
-    # file_path_department = "tempdata/department/2025-05-28/department_12:25:18.164813.json"
-    # file_path_address = "tempdata/address/2025-05-28/address_15:44:01.469582.json"
+def save_to_s3(bucket_name, new_df, transform_name, extract_time):
+    # need to change doc string
+    """
+    Utility function, stores a parquet file in an S3 bucket.  Object key is derived from table_name and extract_time arguments
 
-    # staff_df = pd.read_json(file_path_staff)
-    # department_df = pd.read_json(file_path_department)
-    # address_df = pd.read_json(file_path_address)
+    Args:
+        extract_client (Object): a boto3 client object to query the S3 bucket
+        bucket_name (Object): an S3 bucket name where the JSON object is stored_
+        new_dict_list (_type_): _description_
+        table_name (str): DB table name to be queried
+        extract_time (str) : UTC timestamp of query
 
-    
-    # #method 1 - creates dim_staff table using SQL query/duckdb
-    # dim_staff = duckdb.sql("SELECT staff_df.first_name, staff_df.last_name, department_df.department_name, department_df.location, staff_df.email_address FROM staff_df JOIN department_df ON department_df.department_id = staff_df.department_id").df()
-    
+    Returns:
+        key (str): S3 Object key derived from table_name and extract_time
+    """
 
-    # dim_location = address_df.loc[:,["address_id", "address_line_1", "address_line_2", "district", "city", "postal_code", "country", "phone"]]
-    
+    date, time = extract_time.split('T')
+    key = f"dev/{transform_name}/{date}/{transform_name}_{time}.json"
+    new_df.to_parquet(f"s3://{bucket_name}/dev/{transform_name}/{date}/{transform_name}_{time}.parquet")
+    return key
 
-    # dim_location.to_csv(f"tempdata/dim_location/{date_time}.txt")
+def mvp_transform_df(s3_client, table_name, new_df, processed_bucket):
+    match table_name:
+        case "staff":
+            department_df = key_to_df(s3_client, "department", processed_bucket)
+            dim_staff = pd.merge(new_df, department_df, how= "left", on= "department_id")
+            dim_staff = dim_staff.loc[:,["staff_id", "first_name", "last_name", "department_name", "location", "email_address"]]
+            return "dim_staff", dim_staff
+        case "address":
+            address_df = key_to_df(s3_client, "address", processed_bucket)
+            dim_location = address_df.loc[:,["address_id", "address_line_1", "address_line_2", "district", "city", "postal_code", "country", "phone"]]
+            return "dim_location", dim_location
+        case "counterparty":
+            address_df = key_to_df(s3_client, "address", processed_bucket)
+            dim_counterparty = pd.merge(new_df, address_df, how= "left", left_on="legal_address_id", right_on="address_id")
+            dim_counterparty = dim_counterparty.loc[:, [
+                "counterparty_id", "counterparty_legal_name", "address_line_1", 
+                "address_line_2", "district", "city", "postal_code", "country", "phone"
+                ]
+            ]
+            dim_counterparty.columns = [
+                "counterparty_id", "counterparty_legal_name", "counterparty_legal_address_line_1", 
+                "counterparty_legal_address_line_2", "counterparty_legal_district", "counterparty_legal_city", 
+                "counterparty_legal_postal_code", "counterparty_legal_country", "counterparty_legal_phone_number"
+            ]
+            return "dim_counterparty", dim_counterparty
 
-    # #reads parquet file to check the contents are correct
-    # df=pd.read_parquet(f"tempdata/dim_location/{date_time}.parquet") 
-    # print(df)
-
-    
-    
-    
-# how to name the df correctly
-# what happens if we dont have enough data to complete a table from the df updated - use old df? 
-
-def mvp_transform_to_parquet(table_name, new_df):
-   table_names = {"address":["dim_location"], "counterparty":["dim_counterparty"], 
-                  "currency":["dim_currency"], "department":["dim_staff"], 
-                   "design":["dim_design"], 
-                    "sales_order":["fact_sales_order", "dim_date"],
-                   "staff":["dim_staff"], 
-                }
-    # #method 2 creates dim_staff table using pandas 
-    # dim_staff = pd.merge(staff_df, department_df, how= "inner", on= "department_id")
-    # dim_staff = dim_staff.loc[:,["staff_id", "first_name", "last_name", "department_name", "location", "email_address"]]
-    
-
-    #converts the dataframe to a parquet file and saves to the specified file path
-    # dim_staff.to_parquet(f"tempdata/dim_staff/{date_time}.parquet")
-    # dim_location.to_parquet(f"tempdata/dim_location/{date_time}.parquet")
-   
-   
 def append_json_raw_tables(s3_client, ingestion_bucket, new_json_key, processed_bucket):
         table_name = new_json_key.split('/')[1]
         main_json_key = f"raw_data/{table_name}_all.json"
@@ -130,5 +129,5 @@ def serialise_object(obj):
     raise TypeError("Type not serialisable")
 
     
-events = {'message': 'completed ingestion', 'timestamp': '2025-06-02T11:12:23.510861', 'total_new_files': 11, 'new_keys': ['dev/address/2025-06-02/address_11:12:19.521510.json', 'dev/counterparty/2025-06-02/counterparty_11:12:19.565814.json', 'dev/currency/2025-06-02/currency_11:12:19.606618.json', 'dev/department/2025-06-02/department_11:12:19.646378.json', 'dev/design/2025-06-02/design_11:12:19.715402.json', 'dev/payment/2025-06-02/payment_11:12:20.574726.json', 'dev/payment_type/2025-06-02/payment_type_11:12:21.269205.json', 'dev/purchase_order/2025-06-02/purchase_order_11:12:21.554361.json', 'dev/sales_order/2025-06-02/sales_order_11:12:22.285781.json', 'dev/staff/2025-06-02/staff_11:12:22.779187.json', 'dev/transaction/2025-06-02/transaction_11:12:23.510861.json']}
+events = {'message': 'completed ingestion', 'timestamp': '2025-06-02T11:12:23.510861', 'total_new_files': 1, 'new_keys': ['dev/counterparty/2025-06-02/counterparty_11:12:19.565814.json']}
 lambda_transform(events, None)
