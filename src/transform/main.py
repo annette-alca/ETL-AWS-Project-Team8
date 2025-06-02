67f3ec2a6322a85b9abd9286eae5faa615c29458
annette-alca <alca@me.com>
2025-06-02 21:14:53 +0100

    add some refactoring to generate dim tables

--- src/transform/main.py
+++ src/transform/main.py
@@ -23,8 +23,8 @@ def lambda_transform(events, context):
     list_of_keys = []
     for new_key in events["new_keys"]:
         table_name, new_df = append_json_raw_tables(s3_client, ingestion_bucket, new_key, processed_bucket)
-        transformed_name, transformed_df = mvp_transform_df(s3_client, table_name, new_df, processed_bucket)
-        list_of_keys.append(save_to_s3(processed_bucket, transformed_df, transformed_name, events["timestamp"]))
+        transformed_dict = mvp_transform_df(s3_client, table_name, new_df, processed_bucket)
+        list_of_keys.append(save_to_s3(processed_bucket, transformed_dict, events["timestamp"]))
 
 def key_to_df(s3_client, table_name, bucket_name):
     new_json = s3_client.get_object(Bucket=bucket_name, Key=f"raw_data/{table_name}_all.json")
@@ -32,7 +32,7 @@ def key_to_df(s3_client, table_name, bucket_name):
     new_df.to_csv("tempdata/dim_staff_test.csv")
     return new_df
 
-def save_to_s3(bucket_name, new_df, transform_name, extract_time):
+def save_to_s3(bucket_name, transformed_dict, extract_time):
     # need to change doc string
     """
     Utility function, stores a parquet file in an S3 bucket.  Object key is derived from table_name and extract_time arguments
@@ -47,10 +47,10 @@ def save_to_s3(bucket_name, new_df, transform_name, extract_time):
     Returns:
         key (str): S3 Object key derived from table_name and extract_time
     """
-
-    date, time = extract_time.split('T')
-    key = f"dev/{transform_name}/{date}/{transform_name}_{time}.json"
-    new_df.to_parquet(f"s3://{bucket_name}/dev/{transform_name}/{date}/{transform_name}_{time}.parquet")
+    for transform_name, new_df in transformed_dict.items():
+        date, time = extract_time.split('T')
+        key = f"dev/{transform_name}/{date}/{transform_name}_{time}.json"
+        new_df.to_parquet(f"s3://{bucket_name}/dev/{transform_name}/{date}/{transform_name}_{time}.parquet")
     return key
 
 def mvp_transform_df(s3_client, table_name, new_df, processed_bucket):
@@ -59,11 +59,11 @@ def mvp_transform_df(s3_client, table_name, new_df, processed_bucket):
             department_df = key_to_df(s3_client, "department", processed_bucket)
             dim_staff = pd.merge(new_df, department_df, how= "left", on= "department_id")
             dim_staff = dim_staff.loc[:,["staff_id", "first_name", "last_name", "department_name", "location", "email_address"]]
-            return "dim_staff", dim_staff
+            return {"dim_staff": dim_staff}
         case "address":
             address_df = key_to_df(s3_client, "address", processed_bucket)
             dim_location = address_df.loc[:,["address_id", "address_line_1", "address_line_2", "district", "city", "postal_code", "country", "phone"]]
-            return "dim_location", dim_location
+            return {"dim_location": dim_location}
         case "counterparty":
             address_df = key_to_df(s3_client, "address", processed_bucket)
             dim_counterparty = pd.merge(new_df, address_df, how= "left", left_on="legal_address_id", right_on="address_id")
@@ -77,31 +77,41 @@ def mvp_transform_df(s3_client, table_name, new_df, processed_bucket):
                 "counterparty_legal_address_line_2", "counterparty_legal_district", "counterparty_legal_city", 
                 "counterparty_legal_postal_code", "counterparty_legal_country", "counterparty_legal_phone_number"
             ]
-            return "dim_counterparty", dim_counterparty
+            return {"dim_counterparty": dim_counterparty}
         case "design":
             dim_design = new_df.loc[:,["design_id", "design_name", "file_location", "file_name"]]
-            return "dim_design", dim_design
+            return {"dim_design": dim_design}
         case "currency":
             dim_currency = new_df.loc[:,["currency_id", "currency_code"]]
             currency_dict = {"GBP":"British pound", "USD": "US dollar", "EUR":"Euro","CHF":"Swiss franc"}
             for row in range(len(new_df)):
                 new_df.loc[row,"currency_name"] = currency_dict[new_df.loc[row,"currency_code"]]
-            return "dim_currency", dim_currency
+            return {"dim_currency": dim_currency}
         
         case "sales_order":
-            for row in range(new_df):
-                created_date, created_time = new_df.loc[row,"created_at"].split("T")
-                lastup_date, lastup_time = new_df.loc[row,"last_updated"].split("T")
-                new_df.loc[row,"created_date"] = created_date
-                new_df.loc[row,"created_time"] = created_time
-                new_df.loc[row,"last_updated_date"] = lastup_date
-                new_df.loc[row,"last_updated_time"] = lastup_time
+            for col in ['created_at','last_updated']:
+                new_df[col] = new_df[col].astype(str)
+            new_df[['created_date','created_time']] = new_df['created_at'].str.split(" ", expand=True)
+            new_df[['last_updated_date','last_updated_time']] = new_df['last_updated'].str.split("T", expand=True)
+
             fact_sales_order = new_df.loc[:,["sales_order_id", "created_date", "created_time",
-                            "last_updated_date", "last_updated_time", "sales_staff_id",
-                            "counterparty_id", "units_sold", "unit_price", "currency_id"
-                            "design_id", "agreed_payment_date", "agreed_delivery_date"
+                            "last_updated_date", "last_updated_time", "staff_id",
+                            "counterparty_id", "units_sold", "unit_price", "currency_id",
+                            "design_id", "agreed_payment_date", "agreed_delivery_date",
                             "agreed_delivery_location_id"]]
-            return "fact_sales_order", fact_sales_order
+            fact_sales_order.replace({'staff_id':'sales_staff_id'})
+            print(fact_sales_order)
+
+            dates = pd.concat([new_df['created_date'], new_df['last_updated_date'], 
+                    new_df['agreed_delivery_date'], new_df['agreed_payment_date']], ignore_index=True)
+            dim_date = pd.DataFrame(columns=['date_id','year','month','date'])
+            dim_date['date_id'] = dates
+            dim_date.drop_duplicates(inplace=True, ignore_index=True)
+            dim_date[['year','month','date']] = dim_date['date_id'].str.split("-", expand=True)
+            #still need to add month and day of the week. there is a weekday method in datetime
+            print(dim_date)
+            
+            return {"fact_sales_order": fact_sales_order, "dim_date":dim_date}
 
 
 
@@ -157,5 +167,5 @@ def serialise_object(obj):
     
 events = {'message': 'completed ingestion', 'timestamp': '2025-06-02T16:12:23.510861', 
           'total_new_files': 1, 
-          'new_keys': ['dev/currency/2025-06-02/currency_11:12:19.606618.json']}
+          'new_keys': ['dev/sales_order/2025-06-02/sales_order_13:56:16.189960.json']}
 lambda_transform(events, None)
